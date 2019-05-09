0607dfd217a6a092e5b83df4c8b744e33201895b
Krzysztof Jurkiewicz <huntekah@gmail.com>
2019-05-10 00:43:54 +0200

    add plan for app

--- app.py
+++ app.py
@@ -28,6 +28,10 @@ def receive_message():
                 #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
                 if message['message'].get('text'):
+                    # ANALYZE MESSAGE
+                    # ACT UPON MESSAGE
+                    # GENERATE RESPONSE UPON ACTIONS
+                    # SHOW STATISTICS
                     response_sent_text = get_message()
                     send_message(recipient_id, response_sent_text)
                 #if user sends us a GIF, photo,video, or any other non-text item
